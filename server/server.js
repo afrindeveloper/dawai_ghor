0a8a551f51537c75624406415ac361cd8ad4e724
Aurangzeb Sunny <royestheking@gmail.com>
2026-05-31 21:12:20 +0600

    fix: enforce explicit manual CORS headers to guarantee preflight success

--- server/server.js
+++ server/server.js
@@ -25,13 +25,17 @@ const cookieOptions = {
   secure: process.env.NODE_ENV === 'production'
 };
 
-const allowedOrigins = process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, 'http://localhost:5173'] : ['http://localhost:5173'];
-app.use(cors({
-  origin: function (origin, callback) {
-    callback(null, true);
-  },
-  credentials: true
-}));
+app.use((req, res, next) => {
+  const origin = req.headers.origin || '*';
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
